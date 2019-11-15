a289dafcf894f2936f4a085b319d87246261ebd0
Philipe <phiny1@gmail.com>
2019-11-15 13:46:25 -0200

    fix: initial value set by v-model

--- dist/v-currency-field.common.js
+++ dist/v-currency-field.common.js
@@ -1,5 +1,5 @@
 /*!
- * v-currency-field v3.0.4 
+ * v-currency-field v3.0.5 
  * (c) 2019 Philipe Augusto <phiny1@gmail.com>
  * Released under the MIT License.
  */
@@ -937,7 +937,7 @@ var __vue_staticRenderFns__ = [];
     undefined
   );
 
-var version = '3.0.4';
+var version = '3.0.5';
 
 function install(Vue, globalOptions) {
   if (globalOptions) {
